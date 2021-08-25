444da9d48dda67ee0b4ae840094ac02a3000b301
MARWA HUSSEIN <miroohussein98@gmail.com>
2021-08-25 17:50:54 +0200

    Registration Page #1

--- mengo/lib/main.dart
+++ mengo/lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:mengo/colors/colors.dart';
+import 'package:mengo/ui/registeration/registerationPage.dart';
 void main() {
   runApp(Mengo());
 }
@@ -10,28 +11,7 @@ class Mengo extends StatelessWidget {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       title: 'Mengo',
-      home: MyHomePage(title: 'Mengo'),
-    );
-  }
-}
-
-class MyHomePage extends StatefulWidget {
-  MyHomePage({Key? key, required this.title}) : super(key: key);
-
-  final String title;
-  @override
-  _MyHomePageState createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-
-  @override
-  Widget build(BuildContext context) {
-
-    return Scaffold(
-      appBar: AppBar(
-        backgroundColor: MengoColors.mainColorLight,
-      ),
+      home: Registration(),
     );
   }
 }
